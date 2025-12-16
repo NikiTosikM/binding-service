f1132b2668460c16c1c5e78aed95ca18ee8be5d4
NikiTosikM <marykov63@mail.ru>
2025-12-16 16:31:01 +0300

    create api for contract

--- src/api/contact.py
+++ src/api/contact.py
@@ -1,2 +1,85 @@
+from fastapi import APIRouter
+from fastapi.responses import ORJSONResponse
+
+from src.api.dependencies import RedisClientDep
+from src.services.bussines.contact import ContactService
+from src.schemas.phone_address import PhoneAddressSchema, AddressSchema
 
 
+router = APIRouter(
+    prefix="/contacts",
+    tags=["Работа с данными клиента"],
+    default_response_class=ORJSONResponse,
+)
+
+
+@router.get(
+    "/{phone_number}",
+    description="Получение адреса по номеру телефона",
+    responses={
+        200: {"description": "Адрес найден"},
+        404: {"description": "Данный номер телефона не зарегистрирован"},
+    },
+)
+async def get_client_address(phone_number: str, rd_client: RedisClientDep):
+    contract_service = ContactService(redis_client=rd_client)
+
+    address: str = await contract_service.get_address_by_phone(phone=phone_number)
+
+    return {"address": address}
+
+
+@router.post(
+    "/",
+    description="Привязываем адрес к номеру телефона",
+    responses={
+        201: {"description": "Запись создана"},
+        409: {"description": "Нельзя привязать два адреса к одному номеру телефона"}
+    }
+)
+async def bundle_client_contact(
+    client_contact: PhoneAddressSchema, rd_client: RedisClientDep
+):
+    contract_service = ContactService(redis_client=rd_client)
+
+    await contract_service.bundle_phone_to_address(data=client_contact)
+
+    return {
+        "message": "Запись создана",
+        "phone": client_contact.phone,
+        "address": client_contact.address,
+    }
+
+
+@router.patch(
+    "/{phone_number}", 
+    description="Обновление адреса",
+    responses={
+        200: {"description": "Запись обновлена"},
+        404: {"description": "Данный номер телефона не зарегистрирован"}
+    }
+)
+async def update_address(
+    phone_number: str, address: AddressSchema, rd_client: RedisClientDep
+):
+    contract_service = ContactService(redis_client=rd_client)
+
+    client_contacts = PhoneAddressSchema(phone=phone_number, address=address)
+
+    await contract_service.update_address(data=client_contacts)
+
+    return client_contacts
+
+
+@router.delete(
+    "/{phone_number}", 
+    description="Удаляем контакты клиента",
+    responses={
+        204: {"description": "Запись обновлена"},
+        404: {"description": "Данный номер телефона не зарегистрирован"}
+    }
+)
+async def delete_contact(phone_number: str, rd_client: RedisClientDep):
+    contract_service = ContactService(redis_client=rd_client)
+
+    await contract_service.delete_address_linked_to_phone(phone=phone_number)
--- src/schemas/phone_address.py
+++ src/schemas/phone_address.py
@@ -2,6 +2,13 @@ import re
 from pydantic import BaseModel, ConfigDict, Field, field_validator
 
 
+
+def validate_phone_number(phone: str) -> str:
+    """Общая функция для валидации номера телефона"""
+    if not re.match(r"^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$", phone):
+        raise ValueError("Неверный номер телефона")
+    return phone
+
 class PhoneAddressSchema(BaseModel):
     """ Схема, которая хранит номер телефона клиента и его адрес """
     
@@ -9,13 +16,28 @@ class PhoneAddressSchema(BaseModel):
     address: str = Field(min_length=5, max_length=250)
     
     @field_validator("phone")
-    def validate_phone_number(cls, values: str) -> str:
-        if not re.match(r"^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$", values):
-            raise ValueError("Неверный номер телефона")
-        return values
+    def phone_validator(cls, values: str) -> str:
+        return validate_phone_number(phone=values)
     
     model_config = ConfigDict(
         str_strip_whitespace=True
     )
     
+class PhoneSchema(BaseModel):
+    """ Схема для получения номера телефона """
     
+    phone: str
+    
+    @field_validator("phone")
+    def validate_phone_number(cls, values: str) -> str:
+        return validate_phone_number(phone=values)
+    
+    model_config = ConfigDict(
+        str_strip_whitespace=True
+    )
+    
+
+class AddressSchema(BaseModel):
+    """ Схема для получения адреса"""
+    
+    address: str
